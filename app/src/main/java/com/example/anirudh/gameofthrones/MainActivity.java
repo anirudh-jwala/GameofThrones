794f033882fc25eb5fd6a6e44367485edfcff0e8
anirudh-jwala <anirudhsai2@gmail.com>
2018-06-04 01:36:36 +0530

    After review changes

--- app/src/main/java/com/example/anirudh/gameofthrones/MainActivity.java
+++ app/src/main/java/com/example/anirudh/gameofthrones/MainActivity.java
@@ -6,117 +6,93 @@ import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    // The entire quiz is of 7 points
     int quizScore = 0;
+
+    CheckBox answer1;
+    CheckBox answer2;
+    CheckBox answer3;
+    CheckBox answer4;
+    CheckBox answer5;
+
+    RadioButton q1radioButton;
+    RadioButton q3radioButton;
+    RadioButton q5radioButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
     public void q1checkAnswer(View view) {
-        boolean clicked = ((RadioButton) view).isChecked();
-        switch (view.getId()) {
-            case R.id.q1radioButton1:
-                if (clicked)
-                    Toast.makeText(getBaseContext(), "Wrong Answer!", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.q1radioButton2:
-                if (clicked)
-                    quizScore++;
-                break;
-            case R.id.q1radioButton3:
-                if (clicked)
-                    Toast.makeText(getBaseContext(), "Wrong Answer!", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.q1radioButton4:
-                if (clicked)
-                    Toast.makeText(getBaseContext(), "Wrong Answer!", Toast.LENGTH_SHORT).show();
-                break;
-        }
+        q1radioButton = (RadioButton) findViewById(R.id.q1radioButton2);
+        q1radioButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.q1radioButton2:
+                        if (q1radioButton.isSelected())
+                            quizScore++;
+                        break;
+                }
+            }
+        });
     }
 
     public void q2checkAnswer(View view) {
         EditText text = (EditText) findViewById(R.id.q2_answer);
         String answerValue = text.getText().toString();
-        if (answerValue.equals("The Mountain") || answerValue.equals("Mountain") || answerValue.equals("mountain")) {
+        if (answerValue.equalsIgnoreCase("The Mountain") || answerValue.equalsIgnoreCase("Mountain")) {
             quizScore++;
-        } else {
-            Toast.makeText(getBaseContext(), "Please try Q2 again", Toast.LENGTH_SHORT).show();
         }
     }
 
 
     public void q3checkAnswer(View view) {
-        boolean clicked = ((RadioButton) view).isChecked();
-        switch (view.getId()) {
-            case R.id.q3radioButton1:
-                if (clicked)
-                    Toast.makeText(getBaseContext(), "Wrong Answer!", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.q3radioButton2:
-                if (clicked)
-                    quizScore++;
-                break;
-            case R.id.q3radioButton3:
-                if (clicked)
-                    Toast.makeText(getBaseContext(), "Wrong Answer!", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.q3radioButton4:
-                if (clicked)
-                    Toast.makeText(getBaseContext(), "Wrong Answer!", Toast.LENGTH_SHORT).show();
-                break;
-        }
+        q3radioButton = (RadioButton) findViewById(R.id.q3radioButton2);
+        q3radioButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.q3radioButton2:
+                        if (q3radioButton.isSelected())
+                            quizScore++;
+                        break;
+                }
+            }
+        });
     }
 
     public void q4CheckAnswer(View view) {
-        boolean checked = ((CheckBox) view).isChecked();
-        switch (view.getId()) {
-            case R.id.q4checkBox1:
-                if (checked)
-                    quizScore++;
-                break;
-            case R.id.q4checkBox2:
-                if (checked)
-                    quizScore++;
-                break;
-            case R.id.q4checkBox3:
-                if (checked)
-                    break;
-            case R.id.q4checkBox4:
-                if (checked)
-                    quizScore++;
-                break;
-            case R.id.q4checkBox5:
-                if (checked)
-                    break;
+        answer1 = (CheckBox) findViewById(R.id.q4checkBox1);
+        answer2 = (CheckBox) findViewById(R.id.q4checkBox2);
+        answer3 = (CheckBox) findViewById(R.id.q4checkBox3);
+        answer4 = (CheckBox) findViewById(R.id.q4checkBox4);
+        answer5 = (CheckBox) findViewById(R.id.q4checkBox5);
+
+        if (answer1.isChecked() && answer2.isChecked() && !answer3.isChecked() && answer4.isChecked() && !answer5.isChecked()) {
+            quizScore++;
         }
     }
 
     public void q5CheckAnswer(View view) {
-        boolean clicked = ((RadioButton) view).isChecked();
-        switch (view.getId()) {
-            case R.id.q5radioButton1:
-                if (clicked)
-                    quizScore++;
-                break;
-            case R.id.q5radioButton2:
-                if (clicked)
-                    Toast.makeText(getBaseContext(), "Wrong Answer!", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.q5radioButton3:
-                if (clicked)
-                    Toast.makeText(getBaseContext(), "Wrong Answer!", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.q5radioButton4:
-                if (clicked)
-                    Toast.makeText(getBaseContext(), "Wrong Answer!", Toast.LENGTH_SHORT).show();
-                break;
-        }
+        q5radioButton = (RadioButton) findViewById(R.id.q5radioButton1);
+        q5radioButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.q5radioButton1:
+                        if (q5radioButton.isSelected())
+                            quizScore++;
+                        break;
+                }
+            }
+        });
     }
 
     public void submitButton(View view) {
